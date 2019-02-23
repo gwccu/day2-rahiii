0a5d5b42e01fd3e62c90287d7b287dd4ba6952d4
rahiii <rahimitranyc@gmail.com>
2019-02-23 15:36:43 +0000

    day 3 warmup

---
+++ day3.py
@@ -0,0 +1,7 @@
+print("rahi")
+name = ("rahi")
+print( name + ' mitra')
+print( name * 78)
+# I just printed my name and multiplied my first name 78 times #
+Chicken_Egg = input("What came first, the chicken or the egg?")
+print("you said the" + Chicken_Egg 'came first')
